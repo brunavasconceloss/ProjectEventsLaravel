ad1089001eb829234df616fc9041a11832e04e79
Bruna Vasconcelos <bruna.vasconcelos@sempreceub.com>
2023-06-16 11:01:30 -0300

    Alteração título

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
 <div id="search-container" class="col-md-12">
-    <h1>Busque um evento</h1>
+    <h1>BUSQUE UM EVENTO</h1>
     <form action="/" method="GET">
         <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
     </form>
